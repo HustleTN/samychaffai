9aa1e949c758a504e35006f5601106eceb8d4b9e
HeniHes <heni.es.paypal@gmail.com>
2026-09-07 18:39:19 +0100

    favicon

--- samy-chaffai/app/layout.tsx
+++ samy-chaffai/app/layout.tsx
@@ -15,6 +15,33 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Samy Chaffai",
   description: "Film Director & Content Creator",
+
+  icons: {
+    icon: [
+      {
+        url: "/favicon/favicon.ico",
+      },
+      {
+        url: "/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
